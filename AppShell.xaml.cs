f7ae2320cb5aa0d2415c028a06b0a935674406b0
NERY DAVID MENDEZ <4216528@estudiantes.mined.edu.sv>
2023-07-07 11:09:44 -0600

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace App_superficie_triangulo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace App_superficie_triangulo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
